7d3b3898fe3be70f2d6facf966537f123671c408
SergioD2202 <sergioalejandrods1609@gmail.com>
2021-04-02 05:54:55 +0100

    modif

--- connection.php
+++ connection.php
@@ -1,5 +1,7 @@
 <?php
 //mysql://b3b4d5da09746c:82bd6974@us-cdbr-east-03.cleardb.com/heroku_cfc86fd74ddb0a3?reconnect=true
+
+//mysql -h us-cdbr-east-03.cleardb.com -u b3b4d5da09746c -p heroku_cfc86fd74ddb0a3 < laboratorio.sql 
 define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
 define('DB_USERNAME', 'b3b4d5da09746c');
 define('DB_PASSWORD', '82bd6974');
